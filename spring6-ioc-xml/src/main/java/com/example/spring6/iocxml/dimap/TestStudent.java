e87a7945dfebaaa50c004d38091abcc154740176
linzhuofei <philip.f.lin@gmail.com>
2024-07-09 15:38:06 +0800

    引入外部屬性文件

--- spring6-ioc-xml/src/main/java/com/example/spring6/iocxml/dimap/TestStudent.java
+++ spring6-ioc-xml/src/main/java/com/example/spring6/iocxml/dimap/TestStudent.java
@@ -2,7 +2,7 @@ package com.example.spring6.iocxml.dimap;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class testStudent {
+public class TestStudent {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean-diref.xml");
         Student student = context.getBean("studentP", Student.class);
---
+++ spring6-ioc-xml/src/main/java/com/example/spring6/iocxml/jdbc/TestJdbc.java
@@ -0,0 +1,24 @@
+package com.example.spring6.iocxml.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestJdbc {
+
+    @Test
+    public void demo2(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("bean-jdbc.xml");
+        DruidDataSource druidDataSource = context.getBean(DruidDataSource.class);
+        System.out.println(druidDataSource.getUrl());
+    }
+
+    @Test
+    public void demo1(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    }
+}
