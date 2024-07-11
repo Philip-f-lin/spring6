826bc47a9b5ce48deb270e02b3f14bb0bf569ed4
linzhuofei <philip.f.lin@gmail.com>
2024-07-11 12:12:50 +0800

    基於註解管理Bean-創建Bean對象

---
+++ spring6-ioc-annotation/src/main/java/com/philip/spring6/bean/User.java
@@ -0,0 +1,13 @@
+package com.philip.spring6.bean;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+//@Component(value="user") // 等同於<bean id="user"> class="...">
+//@Repository
+//@Service
+@Controller
+public class User {
+}
