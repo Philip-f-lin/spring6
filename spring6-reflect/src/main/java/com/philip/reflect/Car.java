d96b6eb3a759c2240045c067b6889d27d134f83a
linzhuofei <philip.f.lin@gmail.com>
2024-07-12 14:46:44 +0800

    手寫IOC-回顧Java反射(下)

--- spring6-reflect/src/main/java/com/philip/reflect/Car.java
+++ spring6-reflect/src/main/java/com/philip/reflect/Car.java
@@ -8,7 +8,7 @@ public class Car {
     public Car() {
     }
 
-    private Car(String name, int age, String color) {
+    public Car(String name, int age, String color) {
         this.name = name;
         this.age = age;
         this.color = color;
@@ -40,5 +40,14 @@ public class Car {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
--- spring6-reflect/src/main/java/com/philip/reflect/TestCar.java
+++ spring6-reflect/src/main/java/com/philip/reflect/TestCar.java
@@ -3,7 +3,9 @@ package com.philip.reflect;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class TestCar {
 
@@ -46,7 +48,51 @@ public class TestCar {
         Car car2 = c2.newInstance("Toyota", 15, "白色");
         System.out.println(car2);
     }
+
         //3. 獲取屬性
+    @Test
+    public void test03() throws Exception{
+        Class<Car> clazz = Car.class;
+        Car car = clazz.getConstructor().newInstance();
+        // 獲取所有public屬性
+        Field[] fields = clazz.getFields();
+        // 獲取所有屬性(包含私有屬性)
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            declaredField.setAccessible(true);
+            if (declaredField.getName().equals("name")){
+                declaredField.set(car, "Benz");
+            }
+            System.out.println(declaredField.getName());
+            System.out.println(car);
+        }
+
+    }
         //4. 獲取方法
+
+    @Test
+    public void test04() throws Exception{
+        Car car = new Car("保時捷", 20, "藍色");
+        Class<? extends Car> clazz = car.getClass();
+        // 1 public方法
+        Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            // 執行方法 toString
+            if (method.getName().equals("toString")){
+                String invoke = (String) method.invoke(car);
+                System.out.println("toString方法執行：" + invoke);
+            }
+        }
+
+        //2 private 方法
+        Method[] methodsAll = clazz.getDeclaredMethods();
+        for (Method method : methodsAll) {
+            // 執行方法 run
+            if (method.getName().equals("run")){
+                method.setAccessible(true);
+                method.invoke(car);
+            }
+        }
+    }
 
 }
